2e84a1b5dfd4d765acb7eb8bf1b1df824df9c57a
K V <vinay.k-v@siemens.com>
2025-11-23 20:46:50 +0530

    feat(search): search products added with JPQL query

--- src/main/java/com/telusko/ecom_proj/controller/ProductController.java
+++ src/main/java/com/telusko/ecom_proj/controller/ProductController.java
@@ -90,4 +90,11 @@ public class ProductController {
             }
     }
 
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword) {
+        System.out.println("Searching products with keyword: " + keyword);
+        List<Product> products = service.searchProducts(keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/telusko/ecom_proj/service/ProductService.java
+++ src/main/java/com/telusko/ecom_proj/service/ProductService.java
@@ -62,4 +62,8 @@ public class ProductService {
     public void deleteProduct(int id) {
         repo.deleteById(id);
     }
+
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProducts(keyword);
+    }
 }
